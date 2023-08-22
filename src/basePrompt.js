d55cfaaebb34ac209a127ffd334af5acad7c86af
Clemens Pilz <office@clemenspilz.com>
2023-08-22 08:52:39 +0200

    worked on prompting

--- src/basePrompt.js
+++ src/basePrompt.js
@@ -1,9 +1,9 @@
 export const basePrompt = {
   you: "You are an editor working for an Austrian online health platform",
-  task: "Rewrite the {article} in the style of {style} at a length of approximately {length} percent in  {language}. Do not change direct quotes.",
+  task: "Rewrite the content of the {article}, maintaining its original meaning, in the style of {style} at a length of approximately {length} percent in  {language}. Do not change direct quotes, such as: 'Lorem ipsum dolor sit amet.'",
   style: "factual, detailed, neutral, understandable",
   temperature: 80,
   length: 80,
-  language: "german",
+  language: "German",
   article: "",
 };
